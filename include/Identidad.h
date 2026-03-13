b39c996510788bfa39ae275af48d007f72a3ec9f
Sergio Rodriguez <srodriguez36@outlook.com>
2026-03-12 19:14:10 -0600

    Agregacion de cabeceras para el modulo identidad

--- include/Identidad.h
+++ include/Identidad.h
@@ -1,3 +1,8 @@
 #pragma once
 
+std::string getMAC(const std::string& iface);
+std::string getIP(const std::string& iface);
+std::string getTimestamp();
+void logEvent(const std::string& msg);
+
 void iniciarIdentidad();
